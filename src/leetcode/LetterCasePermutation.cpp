5524141652dafc73c2113aab77e56c3c7d263ce6
Soojung1 <ssj15987@naver.com>
2018-07-13 17:46:18 +0900

    수정

--- src/leetcode/LetterCasePermutation.cpp
+++ src/leetcode/LetterCasePermutation.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     void changeStr(vector<string>& output, string S, int idx){
-        if(idx == 0){
-            output.push_back(S);
-        }
         if(idx > S.size()){
             return;
+        }
+
+        if(idx == 0){
+            output.push_back(S);
         }
         
         changeStr(output, S, idx + 1);
@@ -25,7 +26,7 @@ public:
     vector<string> letterCasePermutation(string S) {
         vector<string> output;
         changeStr(output, S, 0);
-        
+
         sort(output.begin(), output.end());
         output.erase(unique(output.begin(), output.end()), output.end());
         reverse(output.begin(), output.end());
