daf05e19d3da108bcbc96e7177b1ddea6b264c92
Soojung1 <ssj15987@naver.com>
2018-07-16 15:14:47 +0900

    update

--- src/leetcode/MaximumDepthOfBinaryTree.cpp
+++ src/leetcode/MaximumDepthOfBinaryTree.cpp
@@ -9,14 +9,10 @@
  */
 class Solution {
 public:
-    int visit(TreeNode* node, int depth){
-        if(node == NULL){
+    int maxDepth(TreeNode* root) {
+        if(root == NULL){
             return 0;
         }
-        return depth + max(visit(node->left, depth), visit(node->right, depth));
-    }
-    
-    int maxDepth(TreeNode* root) {
-        return visit(root, 1);
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
