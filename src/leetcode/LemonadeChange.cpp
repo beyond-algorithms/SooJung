a730af4879b0ce2e522508d8893dfc6f6d6d2dca
Soojung1 <ssj15987@naver.com>
2018-07-17 14:39:27 +0900

    6회 과제

---
+++ src/leetcode/LemonadeChange.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int money5 = 0;
+        int money10 = 0;
+        
+        int cnt = 0;
+        while(cnt < bills.size()){
+            if(bills[cnt] == 5){
+                money5++;
+            }
+            if(bills[cnt] == 10){
+                money10++;
+                money5--;
+            }
+            if(bills[cnt] == 20){
+                if(money10 > 0){
+                    money10--;
+                    money5--;
+                }
+                else{
+                    money5 -= 3;
+                }
+            }
+            
+            if(money5 < 0){
+                return false;
+            }
+            cnt++;
+        }
+        
+        return true;
+    }
+};
