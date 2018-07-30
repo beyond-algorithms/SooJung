4c6c13657339a8c5d9d706c904d7704883fcc870
Soojung1 <ssj15987@naver.com>
2018-07-30 13:39:50 +0900

    upload

---
+++ src/leetcode/MinimumDistanceBetweenBSTNodes.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void makeArr(TreeNode* root, vector<int>& valArr){
+        if(root != NULL){
+            valArr.push_back(root->val);
+            makeArr(root->left, valArr);
+            makeArr(root->right, valArr);
+        }
+    }
+    
+    int minDiffInBST(TreeNode* root) {
+        vector<int> valArr;
+        makeArr(root, valArr);
+        
+        int minimumDifference = INT_MAX, difference;
+        for(int i = 0; i < valArr.size(); i++){
+            for(int j = i; j < valArr.size(); j++){
+                if(i != j){
+                    difference = abs(valArr[i] - valArr[j]);
+                    if(difference < minimumDifference){
+                        minimumDifference = difference;
+                    }
+                }
+            }
+        }
+        return minimumDifference;
+    }
+};
